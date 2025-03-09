f8fbf6de6d0a478def0ea3e46b445f5b4ab3349b
pinar celik <pcelik157@gmail.com>
2025-03-10 01:37:19 +0300

    Çakışmalar çözüldü

---
+++ resources/views/front/layouts/about.blade.php
@@ -0,0 +1,187 @@
+@extends("front.layouts.app")
+@section("section")
+    <main id="main">
+
+        <!-- ======= Breadcrumbs ======= -->
+        <section id="breadcrumbs" class="breadcrumbs">
+            <div class="container">
+
+                <div class="d-flex justify-content-between align-items-center">
+                    <h2>About</h2>
+                    <ol>
+                        <li><a href="index.html">Home</a></li>
+                        <li>About</li>
+                    </ol>
+                </div>
+
+            </div>
+        </section><!-- End Breadcrumbs -->
+
+        <!-- ======= About Us Section ======= -->
+        <section id="about-us" class="about-us">
+            <div class="container" data-aos="fade-up">
+
+                <div class="row content">
+                    <div class="col-lg-12" data-aos="fade-right">
+                        <h2>Pınar Çelik</h2>
+                        <h3>Yazılım Mühendisi</h3>
+
+
+                        <p>
+                            Ben Pınar Çelik. Fırat Üniversitesi Yazılım Mühendisliği 3. sınıf öğrencisiyim. Web geliştirme, tasarım alanlarına
+                            ve yapay zekaya büyük bir ilgi duyuyorum ve bu doğrultuda kendimi sürekli geliştiriyorum. Laravel üzerine bir
+                            staj deneyimim oldu ve bu süreçte web tasarımı, front-end ve back-end teknolojileri ile ilgili pratik bilgiler
+                            edindim. Gelişime açık, disiplinli ve çözüm odaklı biriyim. Yeni teknolojilere uyum sağlayarak, yazılım alanında
+                            başarılı projeler üretmeyi hedefliyorum. Takım ortamında etkin bir şekilde çalışabilen, iletişim becerileri güçlü
+                            ve proaktif bir yazılım mühendisiyim. Sürekli öğrenme tutkusu sayesinde en son teknolojileri takip ederek
+                            projelere değer katmayı hedefliyorum.
+                        </p>
+                        <ul>
+                            <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequa</li>
+                            <li><i class="ri-check-double-line"></i> Duis aute irure dolor in reprehenderit in voluptate velit</li>
+                            <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in</li>
+                        </ul>
+                        <p class="fst-italic">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                            magna aliqua.
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+        </section><!-- End About Us Section -->
+
+        <!-- ======= Our Team Section ======= -->
+
+
+        <!-- ======= Our Skills Section ======= -->
+        <section id="skills" class="skills">
+            <div class="container">
+
+                <div class="section-title" data-aos="fade-up">
+                    <h2><strong>Skills</strong></h2>
+                    <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+                </div>
+
+                <div class="row skills-content">
+
+                    <div class="col-lg-6" data-aos="fade-up">
+
+                        <div class="progress">
+                            <span class="skill">HTML <i class="val">100%</i></span>
+                            <div class="progress-bar-wrap">
+                                <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </div>
+
+                        <div class="progress">
+                            <span class="skill">CSS <i class="val">90%</i></span>
+                            <div class="progress-bar-wrap">
+                                <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </div>
+
+                        <div class="progress">
+                            <span class="skill">JavaScript <i class="val">45%</i></span>
+                            <div class="progress-bar-wrap">
+                                <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+
+                        <div class="progress">
+                            <span class="skill">PHP <i class="val">80%</i></span>
+                            <div class="progress-bar-wrap">
+                                <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </div>
+
+                        <div class="progress">
+                            <span class="skill">Laravel <i class="val">90%</i></span>
+                            <div class="progress-bar-wrap">
+                                <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </div>
+
+                        <div class="progress">
+                            <span class="skill">Boostrap <i class="val">55%</i></span>
+                            <div class="progress-bar-wrap">
+                                <div class="progress-bar" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+        </section><!-- End Our Skills Section -->
+
+        <!-- ======= Our Clients Section ======= -->
+        <section id="clients" class="clients">
+            <div class="container" data-aos="fade-up">
+
+                <div class="section-title">
+                    <h2>Clients</h2>
+                </div>
+
+                <div class="row no-gutters clients-wrap clearfix" data-aos="fade-up">
+
+                    <div class="col-lg-3 col-md-4 col-6">
+                        <div class="client-logo">
+                            <img src="assets/img/clients/client-1.png" class="img-fluid" alt="">
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-4 col-6">
+                        <div class="client-logo">
+                            <img src="assets/img/clients/client-2.png" class="img-fluid" alt="">
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-4 col-6">
+                        <div class="client-logo">
+                            <img src="assets/img/clients/client-3.png" class="img-fluid" alt="">
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-4 col-6">
+                        <div class="client-logo">
+                            <img src="assets/img/clients/client-4.png" class="img-fluid" alt="">
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-4 col-6">
+                        <div class="client-logo">
+                            <img src="assets/img/clients/client-5.png" class="img-fluid" alt="">
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-4 col-6">
+                        <div class="client-logo">
+                            <img src="assets/img/clients/client-6.png" class="img-fluid" alt="">
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-4 col-6">
+                        <div class="client-logo">
+                            <img src="assets/img/clients/client-7.png" class="img-fluid" alt="">
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-4 col-6">
+                        <div class="client-logo">
+                            <img src="assets/img/clients/client-8.png" class="img-fluid" alt="">
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </section><!-- End Our Clients Section -->
+
+    </main><!-- End #main -->
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,45 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+<<<<<<< HEAD
 Route::get('/', function () {
     return view('welcome');
 });
+=======
+
+
+Route::get('/', function(){
+    return view('front.layouts.app');
+});
+
+Route::get('/blog', function(){
+    return view('front.layouts.blog');
+})->name('blog');
+
+Route::get('/education', function(){
+    return view('front.layouts.education');
+})->name('education');
+
+Route::get('/contact', function(){
+    return view('front.layouts.contact');
+})->name('contact');
+Route::get('/about', function(){
+    return view('front.layouts.about');
+})->name('about');
+
+Route::get('/home', function () {
+    return view('front.layouts.home');
+})->name('home');
+
+Route::get('/hobby', function () {
+    return view('front.layouts.hobby');
+})->name('hobby');
+
+Route::get('/app', function(){
+    return view('panel.layout.app');
+})->name('app');
+
+Route::get('/sertification', function(){
+    return view('front.layouts.sertification');
+})->name('sertification');
+>>>>>>> 247e007 (genel taslak oluşturuldu)
